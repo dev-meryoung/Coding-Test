88c2624d9b465d68ce74ec1c597c8053a349daa8
meryoung <dev.meryoung@gmail.com>
2023-11-18 19:20:50 +0900

    프로그래머스 입문(Level-0) 4문제 풀이
    - 프로그래머스 입문(Level-0) 4문제 풀이

---
+++ Programmers/Level-0/합성수 찾기.js
@@ -0,0 +1,23 @@
+/* 
+  약수의 개수가 세 개 이상인 수를 합성수라고 합니다. 자연수 n이 매개변수로 주어질 때 n이하의 합성수의 개수를 return하도록 solution 함수를 완성해주세요.
+*/
+
+const solution = (n) => {
+  let dap = 0;
+
+  for (let i = 1; i <= n; i++) {
+    let cnt = 0;
+
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) {
+        cnt++;
+      }
+    }
+
+    if (cnt >= 3) {
+      dap++;
+    }
+  }
+
+  return dap;
+};
