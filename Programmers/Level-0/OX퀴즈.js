1062aa0b77c2a5d4f2f371aec985ffe49d241971
meryoung <dev.meryoung@gmail.com>
2023-11-24 22:43:20 +0900

    프로그래머스 입문(Level-0) 4문제 풀이
    - 프로그래머스 입문(Level-0) 4문제 풀이

---
+++ Programmers/Level-0/OX퀴즈.js
@@ -0,0 +1,26 @@
+/* 
+  덧셈, 뺄셈 수식들이 'X [연산자] Y = Z' 형태로 들어있는 문자열 배열 quiz가 매개변수로 주어집니다. 수식이 옳다면 "O"를 틀리다면 "X"를 순서대로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+*/
+
+const solution = (quiz) => {
+  const dap = [];
+
+  quiz.forEach((str) => {
+    const arr = str.split(' ');
+    if (arr[1] === '+') {
+      if (+arr[0] + +arr[2] === +arr[4]) {
+        dap.push('O');
+      } else {
+        dap.push('X');
+      }
+    } else {
+      if (+arr[0] - +arr[2] === +arr[4]) {
+        dap.push('O');
+      } else {
+        dap.push('X');
+      }
+    }
+  });
+
+  return dap;
+};
