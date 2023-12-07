1be003a94bfb813756b1388a27503a9bca14ea2e
meryoung <dev.meryoung@gmail.com>
2023-12-07 23:28:50 +0900

    프로그래머스 기초(Level-0) 10문제 풀이
    - 프로그래머스 기초(Level-0) 10문제 풀이

---
+++ Programmers/Level-0/세로 읽기.js
@@ -0,0 +1,24 @@
+/* 
+  문자열 my_string과 두 정수 m, c가 주어집니다. my_string을 한 줄에 m 글자씩 가로로 적었을 때 왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
+*/
+
+function solution(my_string, m, c) {
+  let str = '';
+  let dap = '';
+  const arr = [];
+
+  [...my_string].forEach((s, idx) => {
+    str += s;
+
+    if ((idx + 1) % m === 0) {
+      arr.push(str);
+      str = '';
+    }
+  });
+
+  arr.forEach((s, idx) => {
+    dap += [...s][c - 1];
+  });
+
+  return dap;
+}
