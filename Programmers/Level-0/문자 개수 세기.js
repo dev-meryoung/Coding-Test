2b63d8a0c5612dd3044246fd3e296a51eefe0af7
meryoung <dev.meryoung@gmail.com>
2023-12-08 15:41:43 +0900

    프로그래머스 기초(Level-0) 10문제 풀이
    - 프로그래머스 기초(Level-0) 10문제 풀이

---
+++ Programmers/Level-0/문자 개수 세기.js
@@ -0,0 +1,71 @@
+/* 
+  알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때, my_string에서 'A'의 개수, my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수, my_string에서 'a'의 개수, my_string에서 'b'의 개수,..., my_string에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+*/
+
+function solution(my_string) {
+  const arr = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  const dap = [];
+
+  dap.length = 52;
+  dap.fill(0, 0, 53);
+
+  [...my_string].forEach((str) => {
+    dap[arr.indexOf(str)] += 1;
+  });
+
+  return dap;
+}
