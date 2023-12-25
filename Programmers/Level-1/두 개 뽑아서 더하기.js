ce5e59eaa48f1fc31fc5cdfdfd31cb3620f8c710
meryoung <dev.meryoung@gmail.com>
2023-12-25 22:11:54 +0900

    프로그래머스 Level-1 3문제 풀이
    - 프로그래머스 Level-1 3문제 풀이

---
+++ Programmers/Level-1/두 개 뽑아서 더하기.js
@@ -0,0 +1,19 @@
+/* 
+  정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(numbers) {
+  const dap = [];
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      const hap = numbers[i] + numbers[j];
+
+      if (dap.indexOf(hap) === -1) {
+        dap.push(hap);
+      }
+    }
+  }
+
+  return dap.sort((a, b) => a - b);
+}
