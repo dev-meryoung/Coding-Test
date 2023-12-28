8fb66bfbba6cda130361047ce52019518757e2b9
meryoung <dev.meryoung@gmail.com>
2023-12-28 17:27:42 +0900

    프로그래머스 Level-1 2문제 풀이
    - 프로그래머스 Level-1 2문제 풀이

---
+++ Programmers/Level-1/소수 만들기.js
@@ -0,0 +1,31 @@
+/* 
+  주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+*/
+
+function solution(nums) {
+  let cnt = 0;
+  let num = 0;
+
+  for (let x = 0; x < nums.length - 2; x++) {
+    for (let y = x + 1; y < nums.length - 1; y++) {
+      for (let z = y + 1; z < nums.length; z++) {
+        let flag = true;
+
+        num = nums[x] + nums[y] + nums[z];
+
+        for (let i = 2; i < num; i++) {
+          if (num % i === 0) {
+            flag = false;
+            break;
+          }
+        }
+
+        if (flag) {
+          cnt++;
+        }
+      }
+    }
+  }
+
+  return cnt;
+}
